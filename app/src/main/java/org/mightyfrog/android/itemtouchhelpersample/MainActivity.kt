0d177eecdfd9abb6551fc771029b2af7c748fa59
mightyfrog <mightyfrog.gc@gmail.com>
2017-12-06 23:50:17 +0900

    lib update + minor code cleanup

--- app/src/main/java/org/mightyfrog/android/itemtouchhelpersample/MainActivity.kt
+++ app/src/main/java/org/mightyfrog/android/itemtouchhelpersample/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<RecyclerView>(R.id.recyclerView).apply {
+        recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = TestAdapter()
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
